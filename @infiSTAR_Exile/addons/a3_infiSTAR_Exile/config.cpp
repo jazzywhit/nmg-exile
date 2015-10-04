b90278df4eb758ce209f02da188064317552be31
Phobix <devin.p.harris85@gmail.com>
2015-10-04 13:18:26 -0400

    added the whole infistar folder and fixed gitignore

---
+++ @infiSTAR_Exile/addons/a3_infiSTAR_Exile/config.cpp
@@ -0,0 +1,25 @@
+/*
+	Author: Chris(tian) "infiSTAR" Lorenzen
+	Contact: infiSTAR23@gmail.com // www.infiSTAR.de
+*/
+class CfgPatches {
+	class a3_infiSTAR_Exile {
+		units[] = {};
+		weapons[] = {};
+		a3_infiSTAR_Exile_version = 0.784;
+		requiredVersion = 1.36;
+		requiredAddons[] = {};
+	};
+};
+class CfgFunctions {
+	class a3_infiSTAR_Exile
+	{
+		class main {
+			file = "a3_infiSTAR_Exile";
+			class preInit {
+				preInit = 1;
+				//	postInit = 1;
+			};
+		};
+	};
+};
