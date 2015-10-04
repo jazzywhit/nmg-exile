5e9f2d102ca1ba5d266e000b0015625a872ea4b1
Phobix <devin.p.harris85@gmail.com>
2015-10-03 23:43:48 -0400

    Installed ace. Infistar is blocking interaction menu

--- Start.bat
+++ Start.bat
@@ -1,10 +1,1 @@
-@echo off
-<<<<<<< HEAD
-start "arma3" /min /high "arma3server.exe" -port=2302 "-config=Config\cfg\Config.cfg" "-profiles=Config\log" "-cfg=Config\cfg\basic.cfg" -name=Exile "-mod=@Exile;@ExileServer;@infiSTAR_Exile;" -world=empty -nosplash -noSound -noPause -enableHT -malloc=system -autoinit
- 
-exit 
-=======
-start "arma3" /min /high "arma3server.exe" -port=2302 "-config=Config\cfg\Config.cfg" "-profiles=Config\log" "-cfg=Config\cfg\basic.cfg" -name=Exile "-mod=@Exile;@ExileServer;@infiSTAR_A3;" -world=empty -nosplash -noSound -noPause -enableHT -malloc=system -autoinit
-
-exit
->>>>>>> 0e6af77cadba74f53d8f7c8ff6f882a2667b7d4f
+start "arma3" /min /high "arma3server.exe" -port=2302 "-config=Config\cfg\Config.cfg" "-profiles=Config\log" "-cfg=Config\cfg\basic.cfg" -name=Exile "-mod=@Exile;@ExileServer;@infiSTAR_Exile;@CBA_A3;@ace;" -world=empty -nosplash -noSound -noPause -enableHT -malloc=system -autoinit
