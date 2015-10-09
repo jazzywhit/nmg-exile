2998a09df89eeba42ec86d832ee11385ff4bb2d5
Phobix <devin.p.harris85@gmail.com>
2015-10-08 20:02:14 -0400

    added zombies and demons

--- Start.bat
+++ Start.bat
@@ -1,1 +1,1 @@
-start "arma3" /min /high "arma3server.exe" -port=2302 "-config=Config\cfg\Config.cfg" "-profiles=Config\log" "-cfg=Config\cfg\basic.cfg" -name=Exile "-mod=@Exile;@ExileServer;@CBA_A3;@ace;" -world=empty -nosplash -noSound -noPause -enableHT -malloc=system -autoinit
+start "arma3" /min /high "arma3server.exe" -port=2302 "-config=Config\cfg\Config.cfg" "-profiles=Config\log" "-cfg=Config\cfg\basic.cfg" -name=Exile "-mod=@Exile;@ExileServer;@CBA_A3;@ace;@Zombies and Demons;" -world=empty -nosplash -noSound -noPause -enableHT -malloc=system -autoinit
