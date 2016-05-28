ac56999d8403c3bc94dd2e18c28304d220c4ec55
Jesse Whitworth <jessecwhitworth@gmail.com>
2016-05-28 18:05:35 -0400

    Removed CUP again

--- @CUP_Terrains_Core/meta.cpp
+++
@@ -1,4 +0,0 @@
-protocol = 1;
-publishedid = 583496184;
-name = "CUP Terrains - Core";
-timestamp = 5247560199260342712;
--- @CUP_Terrains_Maps/meta.cpp
+++
@@ -1,4 +0,0 @@
-protocol = 1;
-publishedid = 583544987;
-name = "CUP Terrains - Maps";
-timestamp = 5247560204319113402;
