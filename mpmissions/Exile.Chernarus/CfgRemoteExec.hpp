ee96ae43633bef2418bd71e900fe3b36281876aa
Jesse Whitworth <jessecwhitworth@gmail.com>
2016-05-29 19:29:06 -0400

    Moved config back to external file and set flight simulation to advanced a3 simulation

--- mpmissions/Exile.Chernarus/CfgRemoteExec.hpp
+++ mpmissions/Exile.Chernarus/CfgRemoteExec.hpp
@@ -6,6 +6,17 @@ class CfgRemoteExec
 		jip = 0;
 		class fnc_AdminReq { allowedTargets=2; };
 		class ExileServer_system_network_dispatchIncomingMessage { allowedTargets=2; };
+
+		// Lockpick
+        class w4_lockpick_fnc_lockpicked { allowedTargets = 2; };
+        class w4_lockpick_fnc_lockpick_attempt { allowedTargets = 2; };
+        class w4_lockpick_fnc_lockpick_failed { allowedTargets = 2; };
+        class w4_lockpick_fnc_create_marker_client { allowedTargets = 1; };
+
+        //AVS
+        class AVS_fnc_rearmVehicle { allowedTargets=2; };
+        class AVS_fnc_refuelVehicle { allowedTargets=2; };
+
 	};
 	class Commands
 	{
