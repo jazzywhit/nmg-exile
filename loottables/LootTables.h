5b95e1c8c486ee85e1a46395e7403d3f9ec66edd
Jesse Whitworth <jessecwhitworth@gmail.com>
2016-06-11 17:32:03 -0400

    Update loot tables to include CUP/HLC/TRYK etc

--- loottables/LootTables.h
+++ loottables/LootTables.h
@@ -64,14 +64,14 @@
 // Spawn Guerilla things :)
 ///////////////////////////////////////////////////////////////////////////////
 > CivillianLowerClass
-30, Trash
+10, Trash
 28, Food
-10, Drinks
-8, Pistols
-5, PistolAmmo
-3, PistolAttachments
-4, SMG
-3, SMGAmmo
+25, Drinks
+24, Pistols
+22, PistolAmmo
+23, PistolAttachments
+20, SMG
+20, SMGAmmo
 3, SMGAttachments
 20, CivilianClothing
 11, CivilianBackpacks
@@ -81,45 +81,55 @@
 1, Restraints
 10, Chemlights
 10, RoadFlares
+15, GuerillaClothing
+14, GuerillaBackpacks
+13, GuerillaVests
+13, GuerillaHeadgear
+20, CUPShotgun
 
 ///////////////////////////////////////////////////////////////////////////////
 // Apartments, Offices etc.
 ///////////////////////////////////////////////////////////////////////////////
 > CivillianUpperClass
-30, Trash
-28, Food
-10, Drinks
+10, Trash
+30, Food
+20, Drinks
 8, Pistols
 5, PistolAmmo
 3, PistolAttachments
-4, SMG
-3, SMGAmmo
-3, SMGAttachments
-3, Rifles
-3, RifleAmmo
-3, RifleAttachments
+14, SMG
+13, SMGAmmo
+13, SMGAttachments
+20, CUPAssaultRifles
+20, CUPAmmunition
+6, RifleAttachments
 20, CivilianClothing
 11, CivilianBackpacks
-5, CivilianVests
+9, CivilianVests
 20, CivilianHeadgear
 10, CivilianItems
 10, Chemlights
+8, DLCOptics
 10, RoadFlares
 1, Restraints
+18, GuerillaClothing
+18, GuerillaBackpacks
+18, GuerillaVests
+18, GuerillaHeadgear
 
 ///////////////////////////////////////////////////////////////////////////////
 // Kiosks, Supermarkets etc.
 ///////////////////////////////////////////////////////////////////////////////
 > Shop
-30, Trash
-15, Food
-15, Drinks
+20, Trash
+25, Food
+25, Drinks
 10, Pistols
 2, PistolAmmo
 1, PistolAttachments
-5, SMG
-1, SMGAmmo
-1, SMGAttachments
+15, SMG
+11, SMGAmmo
+10, SMGAttachments
 5, CivilianBackpacks
 1, CivilianClothing
 3, CivilianHeadgear
@@ -136,11 +146,16 @@
 // Construction Sites, Warehouses, Research etc.
 ///////////////////////////////////////////////////////////////////////////////
 > Industrial
-30, Trash
+20, Trash
 40, IndustrialItems
 25, Vehicle
 15, RoadFlares
 5, Restraints
+10, Pistols
+2, PistolAmmo
+5, SMG
+5, SMGAmmo
+5, SMGAttachments
 
 ///////////////////////////////////////////////////////////////////////////////
 // Factories
@@ -149,57 +164,70 @@
 10, Electronics
 40, Trash
 50, IndustrialItems
+15, SMG
+11, SMGAmmo
+5, SMGAttachments
 
 ///////////////////////////////////////////////////////////////////////////////
 // Fuel Stations, Garages, Workshops etc.
 ///////////////////////////////////////////////////////////////////////////////
 > VehicleService
-30, Trash
+20, Trash
 25, IndustrialItems
 40, Vehicle
 15, RoadFlares
 5, Restraints
+4, SmokeGrenades
+5, Chemlights
+10, CUPShotgun
+10, CUPAmmunition
+8, Pistols
+5, PistolAmmo
+3, PistolAttachments
+17, SMG
+17, SMGAmmo
+17, SMGAttachments
 
 ///////////////////////////////////////////////////////////////////////////////
 // Towers, Barracks, Hangars etc.
 ///////////////////////////////////////////////////////////////////////////////
 > Military
-100, Trash
+20, Trash
 4, CivilianItems
-4, GuerillaItems
-3, HEGrenades
-3, UGLFlares
-3, UGLSmokes
-3, HandGrenades
-3, SmokeGrenades
-2, Restraints
-2, MedicalItems
-5, GuerillaClothing
-5, MilitaryClothing
-4, GuerillaBackpacks
-3, MilitaryBackpacks
-3, GuerillaVests
-3, MilitaryVests
-3, DLCVests
-3, GuerillaHeadgear
-3, MilitaryHeadgear
-1, Ghillies
-1, DLCGhillies
+24, GuerillaItems
+13, HEGrenades
+23, UGLFlares
+23, UGLSmokes
+13, HandGrenades
+23, SmokeGrenades
+5, Restraints
+5, MedicalItems
+25, GuerillaClothing
+35, MilitaryClothing
+24, GuerillaBackpacks
+33, MilitaryBackpacks
+23, GuerillaVests
+33, MilitaryVests
+33, DLCVests
+23, GuerillaHeadgear
+33, MilitaryHeadgear
 1, Rebreathers
-5, Rifles
-3, RifleAmmo
-3, RifleAttachments
-4, LMG
-2, LMGAmmo
-3, Snipers
-2, SniperAmmo
-2, SniperAttachments
-3, DLCRifles
-2, DLCAmmo
-2, DLCOptics
-2, DLCSupressor
-2, Bipods
+39, Rifles
+39, RifleAmmo
+33, RifleAttachments
+25, LMG
+25, LMGAmmo
+23, Snipers
+22, SniperAmmo
+12, SniperAttachments
+22, DLCRifles
+25, DLCAmmo
+17, DLCOptics
+21, DLCSupressor
+20, Bipods
 5, Explosives
+25, CUPSniperRifles
+5, CUPAmmunition
 
 ///////////////////////////////////////////////////////////////////////////////
 // Hospital, Medevac etc. (Does not spawn on Altis!)
@@ -212,19 +240,28 @@
 // Light Houses + Life Guard Towers + Castles
 ///////////////////////////////////////////////////////////////////////////////
 > Tourist
+14, GuerillaItems
+15, GuerillaClothing
+14, GuerillaBackpacks
+13, GuerillaVests
+13, GuerillaHeadgear
 10, MilitaryBackpacks
 10, MilitaryHeadgear
-5, Ghillies
-5, DLCGhillies
-20, Snipers
+25, Food
+25, Drinks
+8, Pistols
+5, PistolAmmo
+3, PistolAttachments
+8, CUPPistols
+4, CUPAmmunition
+1, DLCGhillies
 2, SniperAmmo
 2, SniperAttachments
-20, DLCRifles
 2, DLCAmmo
 2, DLCOptics
 2, DLCSupressor
-4, CivilianItems
+20, CivilianItems
 4, HandGrenades
 4, Restraints
-4, MedicalItems
-4, Explosives
+15, MedicalItems
+1, Explosives
