c1c76f0b6133db987e5432c86f9973edbfc7ddbc
Phobix <devin.p.harris85@gmail.com>
2016-07-08 19:29:32 -0400

    added CBA and BloodLust
    BloodLust seems a bit buggy, needs extensive testing.

--- Start.bat
+++ Start.bat
@@ -4,7 +4,7 @@ title Exile Monitor
 :Serverstart
 echo Launching Server
 echo Exile Server Monitor... Active !
-start "Arma3" /min /wait arma3server.exe "-mod=@exile;" -servermod=@exileserver;@infiSTAR_Exile;@JSRS4APEX; -config=@ExileServer\config.cfg -port=2302 -profiles=SC -cfg=@ExileServer\basic.cfg -name=Config -autoinit
+start "Arma3" /min /wait arma3server.exe "-mod=@exile;" -servermod=@exileserver;@infiSTAR_Exile;@JSRS4APEX;@CBA_A3;@BloodLust; -config=@ExileServer\config.cfg -port=2302 -profiles=SC -cfg=@ExileServer\basic.cfg -name=Config -autoinit
 ping 127.0.0.1 -n 15 >NUL
 echo Exile Server Shutdown ... Restarting!
 ping 127.0.0.1 -n 5 >NUL
